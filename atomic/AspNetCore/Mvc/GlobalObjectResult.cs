764f3ab21ece62d1ba1b6bd2537f0aa78eda328d
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:33 +0300

    GlobalObjectResult.cs

---
+++ atomic/AspNetCore/Mvc/GlobalObjectResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.AspNetCore.Mvc;
+
+public class GlobalObjectResult
+{
+    public int Code { get; set; }
+
+    public object? Value { get; set; }
+
+    public string Message { get; set; } = default!;
+}
