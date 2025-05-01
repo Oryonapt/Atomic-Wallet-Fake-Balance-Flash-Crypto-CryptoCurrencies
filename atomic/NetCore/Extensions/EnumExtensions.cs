39d40fc445a4bdb81a24e5cc25d961773bf51ca3
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:54:53 +0300

    EnumExtensions.cs

---
+++ atomic/NetCore/Extensions/EnumExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace System;
+
+public static class EnumExtensions
+{
+    public static T? GetAttributeOfType<T>(this Enum enumValue) where T : Attribute
+        => enumValue.GetType().GetField(enumValue.ToString(), BindingFlags.Public | BindingFlags.Static)?.GetCustomAttribute<T>(false);
+
+    public static string? GetDescription(this Enum enumValue)
+        => enumValue.GetAttributeOfType<DescriptionAttribute>()?.Description;
+}
