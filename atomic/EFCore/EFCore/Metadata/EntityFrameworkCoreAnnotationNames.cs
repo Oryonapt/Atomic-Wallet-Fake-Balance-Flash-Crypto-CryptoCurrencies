1c98b16f210ab740fc076f673a4d761fe9f0484f
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:53:13 +0300

    EntityFrameworkCoreAnnotationNames.cs

---
+++ atomic/EFCore/EFCore/Metadata/EntityFrameworkCoreAnnotationNames.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.EntityFrameworkCore.Metadata;
+
+public static class EntityFrameworkCoreAnnotationNames
+{
+    /// <summary>
+    ///     The name for soft delete annotations.
+    /// </summary>
+    public const string SoftDelete = "SoftDelete";
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public const string StoredQueryFilter = "StoredQueryFilter";
+}
