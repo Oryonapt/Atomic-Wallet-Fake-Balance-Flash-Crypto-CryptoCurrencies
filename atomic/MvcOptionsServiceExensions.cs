04817b3306d2cecce60417a636b967233fcf25e4
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:51:58 +0300

    MvcOptionsServiceExensions.cs

---
+++ atomic/MvcOptionsServiceExensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class MvcOptionsServiceExensions
+{
+    public static IServiceCollection ConfigureMvcOptions(this IServiceCollection services)
+        => services.ConfigureGlobalResult().Configure<MvcOptions>(options =>
+        {
+            options.Filters.Add<GlobalResultFilterAttribute>();
+            options.Filters.Add<GlobalExceptionFilterAttribute>();
+            options.ModelMetadataDetailsProviders.Add(new ValidationMetadataLocalizationProvider());
+        });
+}
