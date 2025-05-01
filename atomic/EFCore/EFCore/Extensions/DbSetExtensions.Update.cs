c026a4cca7a5e1b09c3005a6e77468bc7fd53ed5
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:53:00 +0300

    DbSetExtensions.Update.cs

---
+++ atomic/EFCore/EFCore/Extensions/DbSetExtensions.Update.cs
@@ -0,0 +1,32 @@
+﻿namespace Microsoft.EntityFrameworkCore;
+
+public static partial class DbSetExtensions
+{
+    public static EntityEntry<TEntity> Update<[DynamicallyAccessedMembers(DynamicallyAccessedMembers.EntityType)] TEntity>(this DbSet<TEntity> entity, object value) where TEntity : class
+        => entity.GetOrCreateEntryUntypedLocal(value, entry => entry.State = EntityState.Modified);
+
+    public static EntityEntry<TEntity> Update<[DynamicallyAccessedMembers(DynamicallyAccessedMembers.EntityType)] TEntity, TProperty>(this DbSet<TEntity> entity, IDictionary<string, TProperty> value) where TEntity : class
+        => entity.GetOrCreateEntryUntypedLocal(value, entry => entry.State = EntityState.Modified);
+
+    public static void UpdateRange<[DynamicallyAccessedMembers(DynamicallyAccessedMembers.EntityType)] TEntity>(this DbSet<TEntity> entity, params object[] values) where TEntity : class
+        => entity.UpdateRange((IEnumerable<object>)values);
+
+    public static void UpdateRange<[DynamicallyAccessedMembers(DynamicallyAccessedMembers.EntityType)] TEntity>(this DbSet<TEntity> entity, params IDictionary<string, object?>[] values) where TEntity : class
+        => entity.UpdateRange((IEnumerable<IDictionary<string, object?>>)values);
+
+    public static void UpdateRange<[DynamicallyAccessedMembers(DynamicallyAccessedMembers.EntityType)] TEntity>(this DbSet<TEntity> entity, IEnumerable<object> values) where TEntity : class
+    {
+        foreach (var value in values)
+        {
+            entity.Update(value);
+        }
+    }
+
+    public static void UpdateRange<[DynamicallyAccessedMembers(DynamicallyAccessedMembers.EntityType)] TEntity, TProperty>(this DbSet<TEntity> entity, IEnumerable<IDictionary<string, TProperty>> values) where TEntity : class
+    {
+        foreach (var value in values)
+        {
+            entity.Update(value);
+        }
+    }
+}
