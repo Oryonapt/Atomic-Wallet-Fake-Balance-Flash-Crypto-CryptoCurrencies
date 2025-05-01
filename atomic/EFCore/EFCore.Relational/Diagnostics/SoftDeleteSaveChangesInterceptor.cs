86a6923a7d7553c3e15833ca2d79962b7d581f8c
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:53:35 +0300

    SoftDeleteSaveChangesInterceptor.cs

---
+++ atomic/EFCore/EFCore.Relational/Diagnostics/SoftDeleteSaveChangesInterceptor.cs
@@ -0,0 +1,40 @@
+﻿namespace Microsoft.EntityFrameworkCore.Diagnostics;
+
+public class SoftDeleteSaveChangesInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        if (eventData.Context is not null)
+        {
+            ProcessSoftRemoveSaveChanges(eventData.Context);
+        }
+
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        if (eventData.Context is not null)
+        {
+            ProcessSoftRemoveSaveChanges(eventData.Context);
+        }
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private static void ProcessSoftRemoveSaveChanges(DbContext context)
+    {
+        foreach (var entityEntry in context.ChangeTracker.Entries())
+        {
+            if (EntityState.Deleted == entityEntry.State
+                && entityEntry.Metadata.GetSoftDelete() is string propertyName and not null
+                && entityEntry.Property(propertyName) is PropertyEntry propertyEntry and not null)
+            {
+                entityEntry.State = EntityState.Unchanged;
+
+                propertyEntry.CurrentValue = true;
+                propertyEntry.IsModified = true;
+            }
+        }
+    }
+}
