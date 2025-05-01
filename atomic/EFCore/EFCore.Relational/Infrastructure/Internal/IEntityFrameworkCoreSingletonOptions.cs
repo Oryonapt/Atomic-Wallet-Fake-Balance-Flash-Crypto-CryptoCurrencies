c91019aea44725e993cdc3db7034fd9d90c363fd
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:53:51 +0300

    IEntityFrameworkCoreSingletonOptions.cs

---
+++ atomic/EFCore/EFCore.Relational/Infrastructure/Internal/IEntityFrameworkCoreSingletonOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+public interface IEntityFrameworkCoreSingletonOptions : ISingletonOptions
+{
+    public bool EnableForeignKeyIndex { get; set; }
+
+    public bool EnableForeignKeyConstraint { get; set; }
+
+    public IEnumerable<string> XmlCommentPath { get; set; }
+
+    public SoftDeleteOptions SoftDeleteOptions { get; set; }
+}
