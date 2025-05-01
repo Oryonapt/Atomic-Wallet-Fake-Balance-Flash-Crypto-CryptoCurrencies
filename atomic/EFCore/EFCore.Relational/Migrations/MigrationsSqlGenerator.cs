c23498ac2034f8d075d537827745734ff6b50cd3
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:54:03 +0300

    MigrationsSqlGenerator.cs

---
+++ atomic/EFCore/EFCore.Relational/Migrations/MigrationsSqlGenerator.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Microsoft.EntityFrameworkCore.Migrations;
+
+public class MigrationsSqlGenerator<TMigrationsSqlGenerator>(
+    MigrationsSqlGeneratorDependencies dependencies, TMigrationsSqlGenerator migrationsSqlGenerator)
+    : MigrationsSqlGenerator(dependencies), IMigrationsSqlGenerator where TMigrationsSqlGenerator : IMigrationsSqlGenerator
+{
+    public override IReadOnlyList<MigrationCommand> Generate(IReadOnlyList<MigrationOperation> operations, IModel? model = null, MigrationsSqlGenerationOptions options = MigrationsSqlGenerationOptions.Default)
+    {
+        foreach (var item in operations.OfType<CreateTableOperation>())
+        {
+            item.ForeignKeys.Clear();
+        }
+        return migrationsSqlGenerator.Generate(operations, model, options);
+    }
+}
