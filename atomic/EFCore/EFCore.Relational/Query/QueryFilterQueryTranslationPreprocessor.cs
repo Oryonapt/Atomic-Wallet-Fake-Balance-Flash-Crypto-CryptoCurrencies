c857f49edbb89dbfea52d7c5e39f7d2b940ac4ec
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:54:06 +0300

    QueryFilterQueryTranslationPreprocessor.cs

---
+++ atomic/EFCore/EFCore.Relational/Query/QueryFilterQueryTranslationPreprocessor.cs
@@ -0,0 +1,18 @@
+﻿namespace Microsoft.EntityFrameworkCore.Query;
+
+public class QueryFilterQueryTranslationPreprocessor(
+    QueryTranslationPreprocessorDependencies dependencies,
+    QueryCompilationContext queryCompilationContext,
+    QueryTranslationPreprocessor innerQueryTranslationPreprocessor)
+        : QueryTranslationPreprocessor(dependencies, queryCompilationContext)
+{
+    public override Expression Process(Expression query)
+    {
+        query = new QueryFilterExpressionVisitor(
+            QueryCompilationContext, 
+            Dependencies.EvaluatableExpressionFilter)
+                .ApplyStoredQueryFilter(query);
+
+        return innerQueryTranslationPreprocessor.Process(query);
+    }
+}
