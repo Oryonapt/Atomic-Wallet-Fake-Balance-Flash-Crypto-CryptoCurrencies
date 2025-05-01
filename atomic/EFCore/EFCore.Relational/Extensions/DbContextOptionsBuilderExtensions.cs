c0382a60edb0423f3511db8503831597cd1bed1a
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:53:40 +0300

    DbContextOptionsBuilderExtensions.cs

---
+++ atomic/EFCore/EFCore.Relational/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+namespace Microsoft.EntityFrameworkCore;
+
+public static class DbContextOptionsBuilderExtensions
+{
+    public static DbContextOptionsBuilder UsePeachol(
+        this DbContextOptionsBuilder optionsBuilder,
+        Action<EntityFrameworkCoreDbContextOptionsBuilder>? dbContextOptionsAction = null)
+    {
+        var extension = GetOrCreateExtension(optionsBuilder);
+        ((IDbContextOptionsBuilderInfrastructure)optionsBuilder).AddOrUpdateExtension(extension);
+
+        dbContextOptionsAction?.Invoke(new EntityFrameworkCoreDbContextOptionsBuilder(optionsBuilder));
+
+        return optionsBuilder;
+    }
+
+    private static EntityFrameworkCoreDbContextOptionsExtension GetOrCreateExtension(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.Options.FindExtension<EntityFrameworkCoreDbContextOptionsExtension>()
+            ?? new EntityFrameworkCoreDbContextOptionsExtension();
+}
