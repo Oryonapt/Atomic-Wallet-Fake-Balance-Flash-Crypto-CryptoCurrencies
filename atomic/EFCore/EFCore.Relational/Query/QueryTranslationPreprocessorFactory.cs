c8e47dedbfe62dbaf4be79041f0b48e233129cbd
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:54:08 +0300

    QueryTranslationPreprocessorFactory.cs

---
+++ atomic/EFCore/EFCore.Relational/Query/QueryTranslationPreprocessorFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.EntityFrameworkCore.Query;
+
+public class QueryTranslationPreprocessorFactory<TQueryTranslationPreprocessorFactory>(
+    QueryTranslationPreprocessorDependencies dependencies,
+    TQueryTranslationPreprocessorFactory innerQueryTranslationPreprocessorFactory)
+        : IQueryTranslationPreprocessorFactory
+            where TQueryTranslationPreprocessorFactory : IQueryTranslationPreprocessorFactory
+{
+    public QueryTranslationPreprocessor Create(QueryCompilationContext queryCompilationContext)
+        => new QueryFilterQueryTranslationPreprocessor(
+            dependencies,
+            queryCompilationContext,
+            innerQueryTranslationPreprocessorFactory.Create(queryCompilationContext));
+}
