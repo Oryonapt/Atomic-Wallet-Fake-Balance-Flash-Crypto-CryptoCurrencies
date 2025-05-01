9826050a21c625903da7bda621deb90907f41ee4
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:53:49 +0300

    EntityFrameworkCoreSingletonOptions.cs

---
+++ atomic/EFCore/EFCore.Relational/Infrastructure/Internal/EntityFrameworkCoreSingletonOptions.cs
@@ -0,0 +1,28 @@
+﻿namespace Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+public class EntityFrameworkCoreSingletonOptions : IEntityFrameworkCoreSingletonOptions
+{
+    public bool EnableForeignKeyIndex { get; set; }
+
+    public bool EnableForeignKeyConstraint { get; set; }
+
+    public IEnumerable<string> XmlCommentPath { get; set; } = [];
+
+    public SoftDeleteOptions SoftDeleteOptions { get; set; } = new SoftDeleteOptions();
+
+    public void Initialize(IDbContextOptions options)
+    {
+        var metioCoreOptionsExtension = options.FindExtension<EntityFrameworkCoreDbContextOptionsExtension>();
+
+        if (null != metioCoreOptionsExtension)
+        {
+            XmlCommentPath = metioCoreOptionsExtension.XmlCommentPath;
+            SoftDeleteOptions = metioCoreOptionsExtension.SoftDeleteOptions;
+            EnableForeignKeyIndex = metioCoreOptionsExtension.EnableForeignKeyIndex;
+        }
+    }
+
+    public void Validate(IDbContextOptions options)
+    {
+    }
+}
