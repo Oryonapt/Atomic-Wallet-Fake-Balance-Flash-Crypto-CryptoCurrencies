a251f2920e6605a0fdd4ff92cccc9d616912f3c8
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:53:54 +0300

    ColumnDefaultValueConvention.cs

---
+++ atomic/EFCore/EFCore.Relational/Metadata/Conventions/ColumnDefaultValueConvention.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+internal sealed class ColumnDefaultValueConvention(ProviderConventionSetBuilderDependencies dependencies) : PropertyAttributeConventionBase<DefaultValueAttribute>(dependencies)
+{
+    protected override void ProcessPropertyAdded(
+       IConventionPropertyBuilder propertyBuilder,
+       DefaultValueAttribute attribute,
+       MemberInfo clrMember,
+       IConventionContext context)
+       => propertyBuilder.HasDefaultValue(attribute.Value, fromDataAnnotation: true);
+}
