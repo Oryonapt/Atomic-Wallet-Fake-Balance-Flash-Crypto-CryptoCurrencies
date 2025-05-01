af800d316c5f4f9541be68895b9fdf791d83d458
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:39 +0300

    ValidationMetadataLocalizationProvider.cs

---
+++ atomic/AspNetCore/Mvc/ModelBinding/Validation/ValidationMetadataLocalizationProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+public class ValidationMetadataLocalizationProvider : IValidationMetadataProvider
+{
+    public void CreateValidationMetadata(ValidationMetadataProviderContext context)
+    {
+        var requiredAttribute = context.ValidationMetadata.ValidatorMetadata.OfType<RequiredAttribute>().FirstOrDefault();
+        if (requiredAttribute != null && string.IsNullOrWhiteSpace(requiredAttribute.ErrorMessage))
+        {
+        }
+    }
+}
