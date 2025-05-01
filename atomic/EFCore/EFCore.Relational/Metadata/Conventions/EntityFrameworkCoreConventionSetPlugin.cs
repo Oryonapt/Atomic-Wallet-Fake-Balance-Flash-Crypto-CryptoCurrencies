1923d334a267067822e9f32a6663cf2d68a727d7
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:54:00 +0300

    EntityFrameworkCoreConventionSetPlugin.cs

---
+++ atomic/EFCore/EFCore.Relational/Metadata/Conventions/EntityFrameworkCoreConventionSetPlugin.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+namespace Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+public class EntityFrameworkCoreConventionSetPlugin(
+    IEntityFrameworkCoreSingletonOptions entityFrameworkCoreSingletonOptions,
+    ProviderConventionSetBuilderDependencies dependencies) : IConventionSetPlugin
+{
+    protected ProviderConventionSetBuilderDependencies Dependencies { get; } = dependencies;
+
+    public virtual ConventionSet ModifyConventions(ConventionSet conventionSet)
+    {
+        if (!entityFrameworkCoreSingletonOptions.EnableForeignKeyIndex)
+        {
+            conventionSet.Remove(typeof(ForeignKeyIndexConvention));
+        }
+
+        var columnDefaultValueConvention = new ColumnDefaultValueConvention(Dependencies);
+        conventionSet.PropertyAddedConventions.Add(columnDefaultValueConvention);
+        conventionSet.PropertyFieldChangedConventions.Add(columnDefaultValueConvention);
+
+        var columnDefaultValueSqlConvention = new ColumnDefaultValueSqlConvention(Dependencies);
+        conventionSet.PropertyAddedConventions.Add(columnDefaultValueSqlConvention);
+        conventionSet.PropertyFieldChangedConventions.Add(columnDefaultValueSqlConvention);
+
+        var columnUpdateIgnoreConvention = new ColumnUpdateIgnoreConvention(Dependencies);
+        conventionSet.PropertyAddedConventions.Add(columnUpdateIgnoreConvention);
+        conventionSet.PropertyFieldChangedConventions.Add(columnUpdateIgnoreConvention);
+
+        var columnInsertIgnoreConvention = new ColumnAddIgnoreConvention(Dependencies);
+        conventionSet.PropertyAddedConventions.Add(columnInsertIgnoreConvention);
+        conventionSet.PropertyFieldChangedConventions.Add(columnInsertIgnoreConvention);
+
+        var tableAndColumnCommentConvention = new TableAndColumnCommentConvention(entityFrameworkCoreSingletonOptions);
+        conventionSet.ModelFinalizingConventions.Add(tableAndColumnCommentConvention);
+
+        return conventionSet;
+    }
+}
