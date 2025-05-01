8d8f55f82cb98927d0f98792ea1458146b343336
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:55:16 +0300

    JsonGuidHandlingAttribute.cs

---
+++ atomic/NetCore/Text/Json/Serialization/Attributes/JsonGuidHandlingAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace System.Text.Json.Serialization;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public sealed class JsonGuidHandlingAttribute(JsonGuidHandling jsonGuidHandling) : JsonConverterAttribute
+{
+    public override JsonConverter? CreateConverter(Type typeToConvert)
+        => new JsonGuidConverter(jsonGuidHandling);
+}
