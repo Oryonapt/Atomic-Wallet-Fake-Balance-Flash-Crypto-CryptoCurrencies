54bcc018f6bb4771510d40bc28a93f998e4c9f73
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:54:10 +0300

    EntityFrameworkCoreRelationalTypeMappingSourcePlugin.cs

---
+++ atomic/EFCore/EFCore.Relational/Storage/EntityFrameworkCoreRelationalTypeMappingSourcePlugin.cs
@@ -0,0 +1,6 @@
+﻿namespace Microsoft.EntityFrameworkCore.Storage;
+
+public class EntityFrameworkCoreRelationalTypeMappingSourcePlugin : IRelationalTypeMappingSourcePlugin
+{
+    public RelationalTypeMapping? FindMapping(in RelationalTypeMappingInfo mappingInfo) => null;
+}
